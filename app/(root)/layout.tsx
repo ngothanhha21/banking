71a2fa5709512fbf3ca1202ec2d729dcf0f2fab6
jackbuimplus <jack.bui@mission.plus>
2024-06-08 14:41:23 +0700

    Sidebar Left

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,31 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Adrian", lastName: "JSM" };
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex flex-col size-full">
+        <div className="root-layout">
+          <Image
+            src="/icons/logo.svg"
+            alt="horizon-logo"
+            height={30}
+            width={30}
+          />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
         {children}
+      </div>
     </main>
   );
 }
