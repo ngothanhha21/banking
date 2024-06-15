4524e52a2d23126d29e6bd9952ace83f2d0d231b
jackbuimplus <jack.bui@mission.plus>
2024-06-15 17:33:13 +0700

    Auth Page UI

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,12 @@
+import AuthForm from '@/components/AuthForm'
 import React from 'react'
 
 const SignUp = () => {
   return (
     <div>
-      Sign up
+      <section className='flex-center size-full max-sm:px-6'>
+        <AuthForm type="sign-up"/>
+      </section>
     </div>
   )
 }
---
+++ components/CustomInput.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { FormControl, FormField, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+import { Control, FieldPath } from "react-hook-form";
+import { z } from "zod";
+import { authFormSchema } from "@/lib/utils";
+
+const formScheme = authFormSchema('sign-up')
+
+interface customInput {
+  control: Control<z.infer<typeof formScheme>>;
+  name: FieldPath<z.infer<typeof formScheme>>;
+  label: string;
+  placeholder: string;
+}
+
+const CustomInput = ({ control, name, label, placeholder }: customInput) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <div className="form-item">
+          <FormLabel className="form-label">{label}</FormLabel>
+          <div className="flex flex-col w-full">
+            <FormControl>
+              <Input
+                {...field}
+                placeholder={placeholder}
+                className="input-class"
+                type={name === "password" ? "password" : "text"}
+              />
+            </FormControl>
+            <FormMessage className="form-message mt-2" />
+          </div>
+        </div>
+      )}
+    />
+  );
+};
+
+export default CustomInput;
