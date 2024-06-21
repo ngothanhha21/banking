42400f209a2d8ab185f5464d16ae087771d003e3
jackbuimplus <jack.bui@mission.plus>
2024-06-21 16:35:51 +0700

    Appwirte Authentication

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -20,16 +20,16 @@ import { Input } from "@/components/ui/input";
 import CustomInput from "./CustomInput";
 import { authFormSchema } from "@/lib/utils";
 import { Loader2 } from "lucide-react";
-import SignUp from "@/app/(auth)/sign-up/page";
 import { useRouter } from "next/navigation";
-import { signIn, signUp } from "@/lib/actions/user.actions";
+import { getLoggedInUser, signIn, signUp } from "@/lib/actions/user.actions";
 
 const AuthForm = ({ type }: { type: string }) => {
-  const router = useRouter()
+  const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const formScheme =  authFormSchema(type)
-  // 1. Define your form.
+  // const logginInUser = await getLoggedInUser();
+
+  const formScheme = authFormSchema(type);
   const form = useForm<z.infer<typeof formScheme>>({
     resolver: zodResolver(formScheme),
     defaultValues: {
@@ -38,32 +38,26 @@ const AuthForm = ({ type }: { type: string }) => {
     },
   });
 
-  // 2. Define a submit handler.
   const onSubmit = async (data: z.infer<typeof formScheme>) => {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     setIsLoading(true);
     try {
-      // sign up with appwrite & create plain link token
-      if(type === 'sign-up') {
-        // const newUser = await signUp(data)
-        // setUser(newUser);
+      if (type === "sign-up") {
+        const newUser = await signUp(data);
+        setUser(newUser);
       }
-      if (type === 'sign-in') {
-        // const response = await signIn({
-        //   email: data.email,
-        //   password:  data.password,
-        // })
-
-        // if(response) router.push('/')
+      if (type === "sign-in") {
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        });
+        if (response) router.push("/");
       }
-    } catch (error) { 
+    } catch (error) {
       console.log(error);
     } finally {
       setIsLoading(false);
     }
-
-  }
+  };
 
   return (
     <section className="auth-form">
@@ -97,62 +91,62 @@ const AuthForm = ({ type }: { type: string }) => {
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              {type === 'sign-up' && (
+              {type === "sign-up" && (
                 <>
-                <div className="flex gap-4">
-                  <CustomInput
-                    control={form.control}
-                    name="firstName"
-                    label="First Name"
-                    placeholder="Enter your first name"
-                  />
-                  <CustomInput
-                    control={form.control}
-                    name="lastName"
-                    label="First Name"
-                    placeholder="Enter your last name"
-                  />
-                </div>
+                  <div className="flex gap-4">
+                    <CustomInput
+                      control={form.control}
+                      name="firstName"
+                      label="First Name"
+                      placeholder="Enter your first name"
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name="lastName"
+                      label="First Name"
+                      placeholder="Enter your last name"
+                    />
+                  </div>
                   <CustomInput
                     control={form.control}
                     name="address1"
                     label="Adrress"
                     placeholder="Enter your specific address"
                   />
-                   <CustomInput
+                  <CustomInput
                     control={form.control}
                     name="city"
                     label="City"
                     placeholder="Enter your city"
                   />
-                <div className="flex gap-4">
-                  <CustomInput
-                    control={form.control}
-                    name="state"
-                    label="State"
-                    placeholder="Example: NY"
-                  />
-                  <CustomInput
-                    control={form.control}
-                    name="postalCode"
-                    label="Postal Code"
-                    placeholder="Example: 11101"
-                  />
-                </div>
-                <div className="flex gap-4">
-                  <CustomInput
-                    control={form.control}
-                    name="dateOfBirth"
-                    label="Date Of Birth"
-                    placeholder="YYYY-MM-DD"
-                  />
-                  <CustomInput
-                    control={form.control}
-                    name="ssn"
-                    label="SSN"
-                    placeholder="Example: 1234"
-                  />
-                </div>
+                  <div className="flex gap-4">
+                    <CustomInput
+                      control={form.control}
+                      name="state"
+                      label="State"
+                      placeholder="Example: NY"
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name="postalCode"
+                      label="Postal Code"
+                      placeholder="Example: 11101"
+                    />
+                  </div>
+                  <div className="flex gap-4">
+                    <CustomInput
+                      control={form.control}
+                      name="dateOfBirth"
+                      label="Date Of Birth"
+                      placeholder="YYYY-MM-DD"
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name="ssn"
+                      label="SSN"
+                      placeholder="Example: 1234"
+                    />
+                  </div>
                 </>
               )}
               <CustomInput
@@ -169,27 +163,32 @@ const AuthForm = ({ type }: { type: string }) => {
               />
               <div className="flex flex-col gap-4">
                 <Button type="submit" className="form-btn" disabled={isLoading}>
-                  {isLoading ? 
-                  <>
-                    <Loader2 size={20} className="animate-spin" /> &nbsp; Loading...  
-                  </>
-                  : type === 'sign-in' ? 'Sign In' : 'Sign Up'
-                  }
+                  {isLoading ? (
+                    <>
+                      <Loader2 size={20} className="animate-spin" /> &nbsp;
+                      Loading...
+                    </>
+                  ) : type === "sign-in" ? (
+                    "Sign In"
+                  ) : (
+                    "Sign Up"
+                  )}
                 </Button>
               </div>
-              
             </form>
           </Form>
 
           <footer className="flex justify-center gap-1">
             <p className="text-14 font-normal text-gray-600">
-              {type === 'sign-in'
-              ? `Don't have an account?`
-              : `Already have an account?`
-              }
+              {type === "sign-in"
+                ? `Don't have an account?`
+                : `Already have an account?`}
             </p>
-            <Link href={type === 'sign-in' ? '/sign-up' : '/sign-in'} className="form-link">
-              {type === 'sign-in' ? 'Sign up' : 'Sign in'}
+            <Link
+              href={type === "sign-in" ? "/sign-up" : "/sign-in"}
+              className="form-link"
+            >
+              {type === "sign-in" ? "Sign up" : "Sign in"}
             </Link>
           </footer>
         </>
--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -13,9 +13,7 @@ const BankCard = ({
       <Link href="/" className="bank-card">
         <div className="bank-card_content">
           <div>
-            <h1 className="text-16 text-white font-semibold">
-              {account.name || userName}
-            </h1>
+            <h1 className="text-16 text-white font-semibold">{userName}</h1>
             <p className="font-ibm-plex-serif font-black text-white">
               {formatAmount(account.currentBalance)}
             </p>
@@ -33,11 +31,7 @@ const BankCard = ({
         </div>
 
         <div className="bank-card_icon">
-          <Image 
-            src="/icons/Paypass.svg" 
-            alt="pay" 
-            width={20} 
-            height={24} />
+          <Image src="/icons/Paypass.svg" alt="pay" width={20} height={24} />
           <Image
             src="/icons/mastercard.svg"
             alt="master card"
@@ -47,11 +41,11 @@ const BankCard = ({
           />
         </div>
         <Image
-            src="/icons/lines.png"
-            alt="lines"
-            height={316}
-            width={190}
-            className="absolute top-0 left-0"
+          src="/icons/lines.png"
+          alt="lines"
+          height={316}
+          width={190}
+          className="absolute top-0 left-0"
         />
       </Link>
       {/* COPY */}
